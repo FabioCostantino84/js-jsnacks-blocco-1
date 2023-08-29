f5db2fa8913df38eac044b2e063a42cf0a408d55
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-29 15:47:07 +0200

    add prompt jsnack 2

---
+++ jsnack2/jsnack2.js
@@ -0,0 +1,20 @@
+/* 
+Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+# tools:
+- const/let
+- prompt
+- if/else
+- console log
+*/
+
+// type first word
+let firstWord = prompt('type your name');
+
+// type second word
+let secondWord = prompt('type your surname');
+
+// check which is the shortest word
+//print in console
