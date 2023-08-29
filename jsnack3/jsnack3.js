28df3b6c184bc7c91536cd63f28db4429777f04b
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-29 23:35:17 +0200

    add markup and append

--- jsnack3/jsnack3.js
+++ jsnack3/jsnack3.js
@@ -12,12 +12,19 @@ Il programma stampa la somma di tutti i numeri inseriti.
 */
 
 
-
+const typeNumbers = document.createElement('div');
+const totals = document.createElement('h3');
 let totalCalculation = 0;
 
+
 for (let i = 0; i < 10; i++) {
-    let typeNumber = Number(prompt ('scegli un numero'));
-    //totalCalculation += typeNumber;
-    totalCalculation = totalCalculation + typeNumber;
-  }
+  let typeNumber = Number(prompt('scegli un numero'));
+  //totalCalculation += typeNumber;
+  totalCalculation = totalCalculation + typeNumber;
+
+  typeNumbers.innerHTML += `${typeNumber} + `;
+  totals.innerHTML = `Totale = ${totalCalculation}`;
+}
+document.body.append(typeNumbers);
+document.body.append(totals);
 console.log(totalCalculation);
