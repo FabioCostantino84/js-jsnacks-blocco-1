6b2f6d2c8938c149f0b285d65026e9eeda2fb657
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-29 17:06:14 +0200

    add for cicle ex jsnack3

--- jsnack3/jsnack3.js
+++ jsnack3/jsnack3.js
@@ -2,4 +2,21 @@
 Snack 3:
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
+
+# tools:
+- let/const
+- for cicle
+- prompt
+- console log
+
 */
+
+
+
+let totalCalculation = 0;
+
+for (let i = 0; i < 10; i++) {
+    let typeNumber = Number(prompt ('scegli un numero'))
+    totalCalculation += typeNumber
+  }
+console.log(totalCalculation);
