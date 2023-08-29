a7a6bc7fcc21aa8b80111edab2201f0cf88f6071
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-29 15:28:52 +0200

    finish

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,13 +8,22 @@ Il software stampa il maggiore.
 - prompt
 - const/let
 - for
+- if/else
 - console
 
 */
 
 // enter the first number
-let firstUserNumber = prompt('type dad age')
+let firstUserNumber = prompt('type dad age');
+
 // enter the second number
-let secondUserNumber = prompt('type mom age')
+let secondUserNumber = prompt('type mom age');
 
 // check which of the two numbers is greater and print
+if (firstUserNumber > secondUserNumber) {
+    console.log(firstUserNumber, 'your father is older');
+    
+} else {
+    console.log(secondUserNumber, 'your mom is older')
+    
+}
