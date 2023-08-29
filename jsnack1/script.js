1dd3863d6131321b10ae3baf024b6be510e708e0
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-29 14:53:43 +0200

    initial commit

---
+++ jsnack1/script.js
@@ -0,0 +1,19 @@
+/* 
+
+Snack 1:
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+# tools:
+- prompt
+- const/let
+- for
+- console
+
+*/
+
+// enter the first number
+
+// enter the second number
+
+// check which of the two numbers is greater and print
